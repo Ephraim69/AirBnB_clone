4c8b44606884fe973b9ed647465df6dcbcc3cf68
Ephraim <ephraimigbinosa@gmail.com>
2023-01-02 14:58:37 +0100

    create a loop and add quit command

---
+++ console.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """Command line program"""
+    prompt = "(hbnb) "
+
+    def do_EOF(self, line):
+        """Handles End Of File character.
+        """
+        print()
+        return True
+
+    def do_quit(self, line):
+        """Exits the program.
+        """
+        return True
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
