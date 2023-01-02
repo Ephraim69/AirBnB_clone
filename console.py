bf3a06065baea57595c986cfbf6d68de16356951
Ephraim <ephraimigbinosa@gmail.com>
2023-01-02 15:18:58 +0100

    implement console program

--- console.py
+++ console.py
@@ -7,13 +7,13 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
     def do_EOF(self, line):
-        """Handles End Of File character.
+        """Handles Exiting the program by pressing Ctrl-D.
         """
         print()
         return True
 
     def do_quit(self, line):
-        """Exits the program.
+        """Quit command to exit the program
         """
         return True
 
