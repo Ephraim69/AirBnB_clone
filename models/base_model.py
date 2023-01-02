592ce47661c06cfa80eebcb1fc90d4e21e79d8b0
Ephraim <ephraimigbinosa@gmail.com>
2023-01-02 13:49:42 +0100

    added file_storage module

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import uuid
 from datetime import datetime
+from models import storage
 """BaseModel module that contains a class that defines
 all common attributes/methods for
 other classes.
@@ -30,6 +31,7 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            storage.new(self)
 
     def __str__(self):
         """sets the format for printing a BaseModel object
@@ -43,6 +45,7 @@ class BaseModel():
         """Saves the object to a JSON file
         """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """Returns a dict withb the values of the __dict__ attribute
