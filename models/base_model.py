2845994144d4f0f43302bf0a31f31a8b36e85fdf
Ephraim <ephraimigbinosa@gmail.com>
2023-01-02 12:32:32 +0100

    added basemodel tests

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import uuid
-import datetime
+from datetime import datetime
 """BaseModel module that contains a class that defines
 all common attributes/methods for
 other classes.
@@ -11,13 +11,25 @@ class BaseModel():
     """BaseModel class that defines all common attributes/methods for
     other classes.
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Init method
         sets an instance id and the time attrivutes to the current time
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = self.created_at
+        if len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key == 'created_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
+                elif key == 'updated_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
+                elif key != '__class__':
+                    setattr(self, key, value)
+
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """sets the format for printing a BaseModel object
@@ -30,7 +42,7 @@ class BaseModel():
     def save(self):
         """Saves the object to a JSON file
         """
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """Returns a dict withb the values of the __dict__ attribute
@@ -47,5 +59,7 @@ class BaseModel():
         return my_dict
 
 
-# test = BaseModel()
-# print(test.to_dict())
+print(datetime, type(datetime))
+var = datetime.now()
+print(datetime.now(), type(var))
+print(datetime.now, type(datetime.now))
